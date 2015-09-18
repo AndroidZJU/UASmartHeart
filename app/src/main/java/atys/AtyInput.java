835d4bd60c1bb3edd06584ef155a640f77e53966
fengnanyue <fengnanyue@zju.edu.cn>
2015-09-18 13:00:34 -0600

    Add dateselecter and calcalate age

--- app/src/main/java/atys/AtyInput.java
+++ app/src/main/java/atys/AtyInput.java
@@ -1,29 +1,53 @@
 package atys;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.fengnanyue.uasmartheart.Config;
 import com.fengnanyue.uasmartheart.MainActivity;
 import com.fengnanyue.uasmartheart.R;
+
+import java.util.Calendar;
 
 /**
  * Created by Fernando on 15/9/16.
  */
 public class AtyInput extends Activity implements View.OnClickListener {
     private Button btnBacktoMain;
+    private Button btnSave;
     private TextView daTextView;
+    private TextView tvName;
+    private String savedName;
+    private String selectedDate;
+    private int mYear,mMonth,mDay,age;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aty_input_data);
+
         btnBacktoMain = (Button)findViewById(R.id.btnBacktoMain);
         btnBacktoMain.setOnClickListener(this);
+        tvName = (EditText)findViewById(R.id.tv_name);
+        savedName = Config.getCachedName(AtyInput.this);
+        System.out.println(savedName);
+        tvName.setText(savedName);
         daTextView = (TextView) findViewById(R.id.tv_date);
         daTextView.setOnClickListener(this);
+        daTextView.setText(Config.getCachedDate(AtyInput.this));
+        btnSave=(Button)findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(this);
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
     }
     @Override
     public void onClick(View v) {
@@ -33,10 +57,30 @@ public class AtyInput extends Activity implements View.OnClickListener {
                 startActivity(new Intent(AtyInput.this, MainActivity.class));
                 finish();
                 break;
+            case R.id.tv_date:
+                new DatePickerDialog(AtyInput.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                        String theDate =String.format("%d-%d-%d",year,monthOfYear+1,dayOfMonth);
+                        selectedDate=theDate;
+                        age = (int)((mYear+mMonth/12)-(year+monthOfYear/12));
+                        daTextView.setText(theDate);
+                    }
+                },mYear,mMonth,mDay).show();
+                break;
+
+            case R.id.btnSave:
+                Toast.makeText(AtyInput.this,"Saved successful",Toast.LENGTH_SHORT).show();
+                Config.cacheAge(AtyInput.this,age);
+                Config.cacheName(AtyInput.this, tvName.getText().toString());
+                Config.cacheDate(AtyInput.this,daTextView.getText().toString());
+                break;
+
+
         }
 
     }
-
 
 
 }
--- app/src/main/java/com/fengnanyue/uasmartheart/Config.java
+++ app/src/main/java/com/fengnanyue/uasmartheart/Config.java
@@ -13,6 +13,9 @@ public class Config {
     public static final int RESULT_METHOD2 =1002;
     public static final int RESULT_METHOD3 =1003;
     public static final int RESULT_NONE =1000;
+    private static final String KEY_NAME ="name";
+    private static final String KEY_DATE = "date";
+    private static final String KEY_AGE = "age";
 
 
     public static int getCachedMethod(Context context){
@@ -22,6 +25,34 @@ public class Config {
     public static void cacheMethod(Context context,int method){
         SharedPreferences.Editor e = context.getSharedPreferences(APP_ID, Context.MODE_PRIVATE).edit();
         e.putInt(KEY_METHOD,method);
+        e.commit();
+    }
+
+    public static String getCachedName(Context context){
+        return context.getSharedPreferences(APP_ID,Context.MODE_PRIVATE).getString(KEY_NAME, "");
+    }
+    public static void cacheName(Context context,String name){
+        SharedPreferences.Editor e = context.getSharedPreferences(APP_ID, Context.MODE_PRIVATE).edit();
+        e.putString(KEY_NAME, name);
+        e.commit();
+    }
+
+    public static String getCachedDate(Context context){
+        return context.getSharedPreferences(APP_ID,Context.MODE_PRIVATE).getString(KEY_DATE,"0000-00-00");
+    }
+    public static void cacheDate(Context context,String date){
+        SharedPreferences.Editor e = context.getSharedPreferences(APP_ID, Context.MODE_PRIVATE).edit();
+        e.putString(KEY_DATE,date);
+        e.commit();
+    }
+
+    public static int getCachedAge(Context context){
+        return context.getSharedPreferences(APP_ID,Context.MODE_PRIVATE).getInt(KEY_AGE, 1);
+    }
+
+    public static void cacheAge(Context context,int age){
+        SharedPreferences.Editor e = context.getSharedPreferences(APP_ID, Context.MODE_PRIVATE).edit();
+        e.putInt(KEY_AGE, age);
         e.commit();
     }
 }
